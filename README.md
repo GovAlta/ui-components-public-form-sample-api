1eafe6ec62b610f472e84f20e9b3bc2cc689f106
Chris Olsen <olsen.chris@gmail.com>
2025-02-27 16:07:53 -0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Public Form API
+
+This is an of the API functionality that is needed to allow for integration of the PublicForm components.
+
+## Run on dev machine
+```bash
+npm run dev
+```
---
+++ src/main.ts
@@ -0,0 +1,62 @@
+import { DataTypes, Sequelize } from "sequelize";
+import express from "express";
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "./db.sqlite",
+});
+const PublicForm = sequelize.define("PublicForms", {
+  userId: DataTypes.INTEGER,
+  name: DataTypes.STRING,
+  data: DataTypes.TEXT,
+});
+
+const app = express();
+
+// CORS Headers
+app.use((req, res, next) => {
+  res.setHeaders(new Headers({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers": "*",
+  }))
+  next();
+});
+
+// Parse to json
+app.use(express.json());
+
+app
+  .get("/pf", async(req, res) => {
+    const userId = 1;
+    const data = await PublicForm.findAll({ where: { userId } });
+
+    res.json(data || []);
+    res.status(200);
+  })
+  .put("/pf", async (req, res) => {
+    const userId = 1;
+    const { name, data } = req.body;
+    const item = await PublicForm.findOne({ where: { userId, name } });
+
+    if (item) {
+      await item.update({ data: JSON.stringify(data) });
+      res.status(204);
+    } else {
+      await PublicForm.create({ userId, name, data: JSON.stringify(data) });
+      res.status(201);
+    }
+    res.send();
+  });
+
+app.listen(3000, async () => {
+  await sequelize.sync();
+  const queryInterface = sequelize.getQueryInterface();
+  await queryInterface.addConstraint("PublicForms", {
+    fields: ["userId", "name"],
+    type: "unique",
+    name: "unique_userId_name",
+  })
+
+  console.log("Listening on port 3000");
+});
